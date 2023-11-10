94651e04e1644a73bbacdac3756cfbd875d19366
Saidov Bekzod <sbns1518@gmail.com>
2023-11-10 10:43:13 +0500

    Выполнение задач

---
+++ go.mod
@@ -0,0 +1,3 @@
+module begin1
+
+go 1.21.3
---
+++ main.go
@@ -0,0 +1,17 @@
+// begin1: Дана сторона квадрата a. Найти его периметр P = 4 * a
+
+package main
+
+import "fmt"
+
+func main() {
+
+	var a int
+
+	fmt.Println("Введите сторону a:")
+
+	fmt.Scan(&a)
+
+	fmt.Println("Периметр P равен:", a * 4)
+
+}
